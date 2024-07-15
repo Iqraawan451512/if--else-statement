9b1d461714ebd01cbda9f7b269e3c2f9b03d6ff7
Iqra <148074762+Iqraawan451512@users.noreply.github.com>
2024-07-15 13:21:44 -0700

    its if-else practice

---
+++ OneDrive/Desktop/online class assignment/if-else stat/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let dayOff = "friday";
+if (dayOff == "saturday") {
+    console.log("its weakend");
+}
+else if (dayOff == "monday") {
+    console.log(`${dayOff}  working days starts`);
+}
+else if (dayOff == "friday") {
+    console.log(`${dayOff} is a half day`);
+}
+else {
+    console.log("its not weakend");
+}
---
+++ OneDrive/Desktop/online class assignment/if-else stat/index.ts
@@ -0,0 +1,19 @@
+ let dayOff ="friday";
+ 
+ if ( dayOff == "saturday") {
+    console.log("its weakend"); 
+} 
+else if(dayOff == "monday"){
+console.log(`${dayOff}  working days starts`);
+
+}
+else if(dayOff=="friday"){
+    console.log(`${dayOff} is a half day`);
+    
+}
+
+
+else {
+    console.log("its not weakend");
+    
+}
